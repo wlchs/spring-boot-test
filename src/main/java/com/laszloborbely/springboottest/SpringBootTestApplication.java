2d9246bf9a8a5d964c8919dbc238bfea7c7a1785
Laszlo Borbely <laci.dj@gmail.com>
2021-03-02 16:22:18 +0100

    add BCryptPasswordEncoder Bean to SpringBootTestApplication

--- src/main/java/com/laszloborbely/springboottest/SpringBootTestApplication.java
+++ src/main/java/com/laszloborbely/springboottest/SpringBootTestApplication.java
@@ -2,9 +2,15 @@ package com.laszloborbely.springboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootTestApplication {
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootTestApplication.class, args);
